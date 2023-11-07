5c23e3d31b8e10cac36719ed8a5f3c9d7b23ca38
Jonathan Bucaro <jonathanbucaro7@gmail.com>
2023-11-07 15:37:02 -0600

    Adding project files

---
+++ Ejercicio 2/Persona/Persona/Models/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Persona.Models
+{
+    public class Persona
+    {
+        [Key] public int DUI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        [Display(Name = "Fecha de Nacimiento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime FechaNac { get; set; }
+        public string Direccion { get; set; }
+        public string CorreoE { get; set; }
+    }
+    public class PersonaDBContext : DbContext
+    {
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
